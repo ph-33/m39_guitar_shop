09cefe731e0c8b55f83f4197b85f8fa816b57d6c
DaiDV <dangvandai1992@gmail.com>
2019-07-09 20:55:39 +0700

    Page home: coding get feature products

--- controller/home.php
+++ controller/home.php
@@ -14,7 +14,11 @@ class Home
 {
     public function index(){
         $category_id = filter_input(INPUT_GET, 'category_id');
-        $products = ProductDB::get_products_by_category($category_id);
+        if($category_id !== null){
+            $products = ProductDB::get_products_by_category($category_id);
+        }else{
+            $products = ProductDB::get_feature_products();
+        }
         include 'view/home.php';
     }
 }
--- model/product_db.php
+++ model/product_db.php
@@ -25,4 +25,25 @@ class ProductDB
             display_db_error($error_message);
         }
     }
+
+    public static function get_feature_products()
+    {
+        $db = \Database::getDB();
+        $query = <<<SQL
+        SELECT *
+        FROM products
+        GROUP BY categoryID
+        ORDER BY dateAdded
+SQL;
+        try {
+            $statement = $db->prepare($query);
+            $statement->execute();
+            $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
+            $statement->closeCursor();
+            return $result;
+        } catch (\PDOException $e) {
+            $error_message = $e->getMessage();
+            display_db_error($error_message);
+        }
+    }
 }
